5b99b01dda8ca83c738aa25d6e6787ecc0ed2bfb
JorisPost <jorispost34@gmail.com>
2023-05-18 10:34:50 +0200

    Update juoris + cocktails

--- docs/jorisCards.js
+++ docs/jorisCards.js
@@ -1,26 +1,26 @@
 const timelineCards = [
+    { 
+    point: "",
+    meta: "Leerdoelen voor de Minor",
+    imgSrc: "PersoonlijkJ/2023-04-17 17_05_32-Opdracht smart buisness - diagrams.net.png",
+    imgAlt: "Foto van het diagram",
+    title: "Leerdoelen voor de Minor",
+    desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu."
+    },
+    {
+        point: "",
+        meta: "Competenties",
+        imgSrc: "",
+        imgAlt: "",
+        title: "Competenties",
+        desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu."
+    },
     {
         point: "Week 1",
         meta: "Kick-Off",
         imgSrc: "PersoonlijkJ/IMG-20230216-WA0003.png",
         imgAlt: "Example Image 1",
         title: "Kick-Off",
-        desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu."
-    },
-    {
-        point: "Week 2",
-        meta: "Sprint 1",
-        imgSrc: "https://example.com/image2.jpg",
-        imgAlt: "Example Image 2",
-        title: "Sprint 1",
-        desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu."
-    },
-    {
-        point: "Week 3",
-        meta: "Sprint 2",
-        imgSrc: "https://example.com/image3.jpg",
-        imgAlt: "Example Image 3",
-        title: "Sprint 2",
         desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu."
     }
     ,
@@ -30,7 +30,7 @@ const timelineCards = [
         imgSrc: "https://example.com/image3.jpg",
         imgAlt: "Example Image 3",
         title: "Sprint 2",
-        desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu."
+        desc: 'Tijdens de workshop<a href =Cocktail_pagina.html attributes-list>Klik hier voor de cocktail API</a>'
     }
     ,
     {
