1423ab6c7b9eb5a92d0cae2a8820d7b111727faf
JorisPost <jorispost34@gmail.com>
2023-05-19 11:50:22 +0200

    Update persoolijke pagina joris

--- docs/jorisCards.js
+++ docs/jorisCards.js
@@ -5,40 +5,66 @@ const timelineCards = [
     imgSrc: "PersoonlijkJ/2023-04-17 17_05_32-Opdracht smart buisness - diagrams.net.png",
     imgAlt: "Foto van het diagram",
     title: "Leerdoelen voor de Minor",
-    desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu."
+    desc: "Aan het begin van de minor moesten we voor het vak smart me een lijst met onze leerdoelen opstellen, ik heb de volgende doelen opgeschreven: Het vergroten van mijn netwerk, buiten mensen met in hetzelfde vakgebied. Ook het leren van andermans kennis, tijdens de workshops en tijdens het project. Beide doelen zijn geslaagd, ik heb tijdens mijn project veel tijd gespendeerd met Ryan en Chantal. Tijdens de opdrachten die we moesten doen hebben we alles samen besproken en waar ik vragen had legde zij het uit."
+
+},
+    {
+        point: "",
+        imgSrc: "PersoonlijkJ/FIBER_PCBackgroundV2.png",
+        title: "Competenties",
+        desc: "Bij mijn competenties heb ik staan, de competenties die horen bij de studie van Mechatronica (Dit zijn Mechanica, electrotechniek, software en control enginering). Dit heb ik onder andere toe kunnen passen bij het Nederlands robot team, waarbij we in 5 weken een robot moesten bedenken en realiseren. Daarnaast staat er ook nog de besturing van CNC machines in, dit is iets wat ik heel graag doe en ik heb er ook mijn werk in heb gevonden."
     },
     {
         point: "",
-        meta: "Competenties",
-        imgSrc: "",
-        imgAlt: "",
-        title: "Competenties",
-        desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu."
-    },
-    {
-        point: "Week 1",
-        meta: "Kick-Off",
-        imgSrc: "PersoonlijkJ/IMG-20230216-WA0003.png",
+        meta: "Workshop 1: Hacking your brain",
+        imgSrc: "https://www.oneracademy.com/wp-content/uploads/2021/03/Ilke-31-scaled.jpg",
         imgAlt: "Example Image 1",
         title: "Kick-Off",
-        desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu."
+        desc: "De eerste workshop waar ik een reflectie op wil doen was die van Ilke Oner van de 'Oner Academy'. Het probleem was dat er een aantal mensen de schuld van een probleem op mij af aan het schuiven waren, aangezien zij ouder zijn dan ik wist ik niet hoe ik dit op een respectvolle manier duidelijk moest maken. Samen hebben we het gehad over een hoe ik een punt duidelijk maak zonder respectloss te zijn dit heeft mij goed geholpen in deze situatie."
     }
     ,
     {
-        point: "Week 3",
-        meta: "Sprint 2",
-        imgSrc: "https://example.com/image3.jpg",
+        point: "",
+        meta: "Workshop 2: Kookworkshop",
+        imgSrc: "PersoonlijkJ/IMG-20230216-WA0051.png",
         imgAlt: "Example Image 3",
-        title: "Sprint 2",
-        desc: 'Tijdens de workshop<a href =Cocktail_pagina.html attributes-list>Klik hier voor de cocktail API</a>'
+        title: "Kookworkshop bij het kookcollege",
+        desc: 'gsrdgh '
     }
     ,
     {
-        point: "Week 3",
-        meta: "Sprint 2",
-        imgSrc: "https://example.com/image3.jpg",
+        point: "",
+        meta: "Workshop 3: Smart connection",
+        imgSrc: "https://th.bing.com/th/id/R.f37e1460eac80c223d56b662efc87536?rik=DYFD9JtUdvg9tw&pid=ImgRaw&r=0",
         imgAlt: "Example Image 3",
-        title: "Sprint 2",
-        desc: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu."
+        title: "Workshop 3: ",
+        desc: "Tijdens de workshop van smart connection het ik gebruik leren maken van API's. Als leuke opdracht hadden we verzonnen om een automatische cocktail generator te creeren, deze kiest elke keer als de pagina wordt ververst een nieuwe cocktail uit. Zie hier het resultaat:<a href =Cocktail_pagina.html attributes-list>De cocktail API</a>"
+    }
+    ,
+    {
+        point: "",
+        meta: "Smart project 1: Nieuwe mensen",
+        imgSrc: "PersoonlijkJ/1.png",
+        imgAlt: "Example Image 3",
+        title: "Workshop 3: ",
+        desc: ""
+    }
+    ,
+    {
+        point: "",
+        meta: "Workshop 3: ",
+        imgSrc: "PersoonlijkJ/2023-05-18 11_43_52-Overzicht – Home Assistant and 4 more pages - Profile 1 - Microsoft​ Edge.png",
+        imgAlt: "Example Image 3",
+        title: "Workshop 3: ",
+        desc: "Tijdens de workshop<a href =Cocktail_pagina.html attributes-list>Klik hier voor de cocktail API</a>"
+    }
+    ,
+    {
+        point: "",
+        meta: "Workshop 3: ",
+        imgSrc: "https://heerlijkehappen.nl/wp-content/uploads/2022/08/Klassieke-Cuba-libre-cocktail-7-scaled.jpg",
+        imgAlt: "Example Image 3",
+        title: "Workshop 3: ",
+        desc: "Tijdens de workshop<a href =Cocktail_pagina.html attributes-list>Klik hier voor de cocktail API</a>"
     }
 ];
