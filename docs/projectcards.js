ecdfb69d4ee5385a5c70359e63b8ba8161de72ce
Ryan Aelen <rmm.aelen@student.han.nl>
2023-05-31 16:10:11 +0200

    added cards support to main.html

---
+++ docs/projectcards.js
@@ -0,0 +1,98 @@
+const timelineCards = [
+    { 
+    point: "",
+    meta: "Leerdoelen voor de Minor",
+    imgSrc: "PersoonlijkJ/2023-04-17 17_05_32-Opdracht smart buisness - diagrams.net.png",
+    imgAlt: "Foto van het diagram",
+    title: "Leerdoelen voor de Minor",
+    desc: "Aan het begin van de minor moesten we voor het vak smart me een lijst met onze leerdoelen opstellen, ik heb de volgende doelen opgeschreven: Het vergroten van mijn netwerk, buiten mensen met in hetzelfde vakgebied. Ook het leren van andermans kennis, tijdens de workshops en tijdens het project. Beide doelen zijn geslaagd, ik heb tijdens mijn project veel tijd gespendeerd met Ryan en Chantal. Tijdens de opdrachten die we moesten doen hebben we alles samen besproken en waar ik vragen had legde zij het uit."
+
+},
+    {
+        point: "",
+        imgSrc: "PersoonlijkJ/FIBER_PCBackgroundV2.png",
+        title: "Competenties",
+        desc: "Bij mijn competenties heb ik staan, de competenties die horen bij de studie van Mechatronica (Dit zijn Mechanica, electrotechniek, software en control enginering). Dit heb ik onder andere toe kunnen passen bij het Nederlands robot team, waarbij we in 5 weken een robot moesten bedenken en realiseren. Daarnaast staat er ook nog de besturing van CNC machines in, dit is iets wat ik heel graag doe en ik heb er ook mijn werk in heb gevonden."
+    },
+    {
+        point: "",
+        meta: "Workshop 1: Hacking your brain",
+        imgSrc: "https://www.oneracademy.com/wp-content/uploads/2021/03/Ilke-31-scaled.jpg",
+        imgAlt: "Example Image 1",
+        title: "Kick-Off",
+        desc: "De eerste workshop waar ik een reflectie op wil doen was die van Ilke Oner van de 'Oner Academy'. Het probleem was dat er een aantal mensen de schuld van een probleem op mij af aan het schuiven waren, aangezien zij ouder zijn dan ik wist ik niet hoe ik dit op een respectvolle manier duidelijk moest maken. Samen hebben we het gehad over een hoe ik een punt duidelijk maak zonder respectloss te zijn dit heeft mij goed geholpen in deze situatie."
+    }
+    ,
+    {
+        point: "",
+        meta: "Workshop 2: Kookworkshop",
+        imgSrc: "PersoonlijkJ/IMG-20230216-WA0051.png",
+        imgAlt: "Example Image 3",
+        title: "Kookworkshop bij het kookcollege",
+        desc: 'De kookworkshop bij het Kookcollege was een geweldige ervaring waarbij ik heb samengewerkt met mensen van verschillende disciplines. We hebben binnen de tijd plezier gehad en een heerlijk dessert bereid. Het was een leuke oefening om mijn communicatievaardigheden te verbeteren. Kortom, de kookworkshop bood zowel een smakelijke ervaring als de mogelijkheid om te leren samenwerken en communiceren in een culinaire setting.'
+    }
+    ,
+    {
+        point: "",
+        meta: "Workshop 3: Smart connection",
+        imgSrc: "https://th.bing.com/th/id/R.f37e1460eac80c223d56b662efc87536?rik=DYFD9JtUdvg9tw&pid=ImgRaw&r=0",
+        imgAlt: "Example Image 3",
+        title: "Workshop 3: ",
+        desc: "Tijdens de workshop van smart connection het ik gebruik leren maken van API's. Als leuke opdracht hadden we verzonnen om een automatische cocktail generator te creeren, deze kiest elke keer als de pagina wordt ververst een nieuwe cocktail uit. Zie hier het resultaat:<a href =Cocktail_pagina.html attributes-list>De cocktail API</a>"
+    }
+    ,
+    {
+        point: "",
+        meta: "Smart Technology 1: Ontwerpen",
+        imgSrc: "PersoonlijkJ/2023-04-17 16_54_39-Autodesk Fusion 360 (Education License).png",
+        imgAlt: "Example Image 3",
+        title: "Smart Technology 1: Ontwerpen",
+        desc: "Tijdens het vak smart technology hebben we gewerkt aan een module die de status van een plant kan monitoren. Dit is het resultaat van het ontwerp, om mijzelf uit te dagen had ik ervoor gekozen om het zo compact mogelijk te maken. Het bakje is 60*60*50 mm."
+    }
+    ,
+    {
+        point: "",
+        meta: "Smart Technology 2: Elektronica",
+        imgSrc: "PersoonlijkJ/2023-04-17 16_56_42-Autodesk Fusion 360 (Education License).png",
+        imgAlt: "Example Image 3",
+        title: "Smart Technology 2: Elektronica",
+        desc: "Om de verbindengen tussen de verschillende onderdelen te maken hebben we een PCB ontworpen. Ik heb dit samen met medestudenten te doen. Dit is goed gegaan en alle verschillende onderdelen werken."
+    }
+    ,
+    {
+        point: "",
+        meta: "Smart Technology 3: De software",
+        imgSrc: "PersoonlijkJ/2023-05-20 12_50_59-syn6-ioh4wtzus3bmuwg4wvaqz7qlvifibkisqwxmaadeqj2d774ibjtu.jorispost34.direct.qui.png",
+        imgAlt: "Example Image 3",
+        title: "Smart Technology 3: De software",
+        desc: "Tijdens het programmeren van de ESP heb ik gekozen om mijzelf uit te dagen op een gebied waar ik meer over wilde leren. Dit was het programmeren van databases. Ik wil data kunnen versturen naar een eigen NAS server waar ik de data op kan slaan. Om dit te bereiken heb ik een Raspberry Pi gebruikt waar de ESP verbinding mee maakt, die vervolgens de data naar de NAS stuurt. Dit is gelukt en het resultaat is te zien in de foto hierboven. "
+    }
+    ,
+    {
+        point: "",
+        meta: "Smart Technology 4: De software",
+        imgSrc: "PersoonlijkJ/2023-05-18 13_57_13-Blynk.Console.png",
+        imgAlt: "Example Image 3",
+        title: "Smart Technology 4: De software",
+        desc: "Ook is er nog een live dashboard waar de data live kan worden ingezien. Zie de foto voor het resultaat."
+    }
+    ,
+        {
+        point: "",
+        meta: "Smart Technology 4: De software",
+        imgSrc: "PersoonlijkJ/2023-05-18 13_57_13-Blynk.Console.png",
+        imgAlt: "Example Image 3",
+        title: "Smart Technology 4: De software",
+        desc: "Ook is er nog een live dashboard waar de data live kan worden ingezien. Zie de foto voor het resultaat."
+    }
+    ,
+    {
+        point: "",
+        meta: "Smart Project: De rol binnen het project",
+        imgSrc: "2023-05-17 13_09_12-.png",
+        imgAlt: "Example Image 3",
+        title: "Smart Project: De rol binnen het project",
+        desc: "Het doel van mijn project was het ontwikkelen van een home energy management system dat inzicht biedt in het energieverbruik en de huidige energietarieven. Ik was direct om door het enthousiasme van Pieter tijdens zijn presentatie, en zijn passie sprak me meteen aan. Daarnaast sprak de opdracht mij aan omdat ik het gevoel had dat ik hierdoor nieuwe vaardigheden kon leren die ik nog niet eerder had ontwikkeld.\n\nGedurende het project heb ik het geluk gehad om te leren van twee bijzondere mensen: Chantal en Ryan. Chantal heeft me op het gebied van bedrijfskunde veel bijgebracht en heeft me geholpen bij het opzetten van een bedrijf. Haar kennis en expertise op dit gebied zijn van onschatbare waarde gebleken, en ik weet zeker dat ik hier in de toekomst enorm veel profijt van zal hebben.\n\nAan de andere kant heb ik veel geleerd van Ryan, die een schat aan kennis heeft op het gebied van programmeren en passie heeft voor het delen van die kennis. Hij heeft me versteld doen staan met zijn vermogen om complexe onderwerpen op een begrijpelijke manier uit te leggen. Zijn uitleg heeft me geholpen om mijn programmeervaardigheden naar een hoger niveau te tillen.\nBinnen het project kreeg ik de kans om zowel bij te dragen aan de zakelijke kant als aan het programmeren van ons product. Dit heeft me waardevolle ervaring opgeleverd op het gebied van het organiseren van vergaderingen, het leggen van goede contacten en het ontwikkelen van mijn programmeervaardigheden.\n\nIk ben enorm dankbaar voor de kans om met zulke inspirerende mensen te werken en ik ben trots op wat we als team hebben bereikt. Deze ervaring heeft mijn persoonlijke en professionele groei gestimuleerd en ik ben ervan overtuigd dat de lessen die ik van Chantal en Ryan heb geleerd, een blijvende impact zullen hebben op mijn toekomstige carrière.\n\nDus, Chantal en Ryan, bedankt voor jullie inspiratie, begeleiding en samenwerking. Ik waardeer jullie enorm en ik kijk uit naar onze toekomst. Samen zullen we nog meer geweldige dingen bereiken!"
+    }
+
+];
