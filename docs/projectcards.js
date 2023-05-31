03e96c3468213f8d215ebb6f9cbf540956f85cc6
JorisPost <jorispost34@gmail.com>
2023-05-31 19:49:57 +0200

    1e onderdelen toegevoegd

--- docs/projectcards.js
+++ docs/projectcards.js
@@ -2,10 +2,10 @@ const timelineCards = [
     { 
     point: "",
     meta: "Omschrijving van het project:",
-    imgSrc: "",
-    imgAlt: "Foto van het diagram",
+    imgSrc: "https://www.t-mobile.nl/founders/app/uploads/2018/04/DSCF1379.jpg",
+    imgAlt: "",
     title: "Omschrijving van het project:",
-    desc: ""
+    desc: "Tijdens onze presentatie in het vak Smart Project kregen we onder andere een presentatie van Pieter Hoenderken over het Home Energy Management System. Ons doel was om een systeem te ontwikkelen dat het gebruik van een dynamisch energiecontract aantrekkelijker zou maken door middel van inzicht en geautomatiseerde acties.<br><br>Pieter presenteerde een plan om dit doel te bereiken. Hij legde de nadruk op het belang van bewustwording van energieverbruik en hoe dit verbeterd kan worden met behulp van slimme technologieën. Het Home Energy Management System dat hij voorstelde, bood real-time inzicht in energieverbruik en maakte automatisering van bepaalde acties mogelijk om de energie-efficiëntie te verbeteren."
     }
     ,
     {
@@ -14,7 +14,7 @@ const timelineCards = [
         imgSrc: "",
         imgAlt: "Example Image 1",
         title: "Omschrijving van het project:",
-        desc:  ""
+        desc:  "Het doel van dit project was om een product te ontwikkelen tot Technical Readiness Level 4. Bovendien kregen we een geweldige kans om samen met Pieter een eigen bedrijfje op te richten voor dit product. Ons uiteindelijke doel was om een innovatieve oplossing te creëren die consumenten op eeb gemakkelijke manier helpt om volledige controle te krijgen over hun energieverbruik."
     },
     {
         point: "",
