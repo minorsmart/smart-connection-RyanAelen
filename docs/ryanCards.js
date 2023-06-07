beaf1f9c6dc2926ada24914f2ffc070c76f526fb
Ryan Aelen <rmm.aelen@student.han.nl>
2023-06-07 02:47:56 +0200

    missende punten van de ryan cards bijgewerkt

--- docs/ryanCards.js
+++ docs/ryanCards.js
@@ -5,7 +5,7 @@ var timelineCards2 = [
         "point": "week&nbsp14",
         // "imgSrc": "",
         "imgAlt": "",
-        "desc": "Bij de project van MSI hebben we deels bessi kunnen invulle, omdat we een new product op de markt willen brengen konden we helaas niet veel van invullen en moesten we nog zelf uit vogelen waar we naar toe werken.",
+        "desc": "Bij de project van MSI hebben we deels bessi kunnen invulle, omdat we een new product op de markt willen brengen konden we helaas niet veel van invullen en moesten we nog zelf uit vogelen waar we naar toe werken. Dit maakte voor mij maar ook voor het team duidelijk dat het product nog in kinder schoenen staat en we een hoop werk te gemoet zullen krijgen.",
         "path": "\\BESSI.md"
     },
     {
@@ -21,7 +21,7 @@ var timelineCards2 = [
         "date": "2023-03-14",
         "title": "Brain Hacking met Ilke Oner",
         "point": "week&nbsp11",
-        "imgSrc": "",
+        // "imgSrc": "",
         "imgAlt": "",
         "desc": "De belangrijkse wat ik heb mee gekregen tijdens de brain hacking is hoe je naar andere over brengt en over je zelf zet tegen over andere. Bij de talk en met een op een gesprek met Ilke verdiepen in Rappor. Probleem waar mee ik zat is dat je zelf en de ander persoon op elkaar afstem, dat je eigelijk elkaar zit te missleidinen door te doen of iemand anders ben dan wie je eigelijk bent. Zo voel ik mij er bij. en dit te doorbreken dat het niet zo is, ben ik nog steeds mee bezig daar convertable bij te voelen.",
         "path": "\\Brain Hacking met Ilke Oner.md"
@@ -89,7 +89,9 @@ var timelineCards2 = [
         "imgSrc": "PersoonlijkR/pithces.png",
         "imgAlt": "",
         "desc": `Mijn eigen leerdoelen voor de smart business bestaat uit <ul>
-        <li>Geen aannamens nemen</li>
+        <li>Geen aannamens nemen:</li> Dit doe ik door door tevragen als ik iets niet begreep of heb verstaan.
+        <li>Namen van de studenten weten:</li> Dit doe ik door elke bijeenkomst de namen door te nemen en te oefenen. 
+        <li>Mij zelf verbeteren op in social aspect:</li> Dit doe ik door mij zelf naar voren te brengen.
         </ul>`,
         "path": "\\Smart Busines\\Smarkt busniess.md"
     },
@@ -102,22 +104,22 @@ var timelineCards2 = [
         "desc": "De value propersistion canvas was een canvas waar ik nog vragen bij had, geluk kon ik met de kennis van Chantal extra informatie ontrekken om mij duidelijker te maken. Dit heb ik kunnen gebruiken bij het project van MSI om te kijken welke pijnen handig is om te ontnemen van de klant.",
         "path": "\\Smart Busines\\value proposition canvas.md"
     },
-    {
-        "date": "2023-02-15",
-        "title": "bedrfijs pitches",
-        "point": "week&nbsp07",
-        "imgSrc": "",
-        "imgAlt": "",
-        "desc": "Invullen over bedrijf pihches",
-        "path": "\\Smart Busines\\pitchens\\Agriware.md"
-    },
+    // {
+    //     "date": "2023-02-15",
+    //     "title": "bedrfijs pitches",
+    //     "point": "week&nbsp07",
+    //     "imgSrc": "",
+    //     "imgAlt": "",
+    //     "desc": "Invullen over bedrijf pihches",
+    //     "path": "\\Smart Busines\\pitchens\\Agriware.md"
+    // },
     {
         "date": "2023-02-08",
         "title": "Smart connect eerte workshop",
         "point": "week&nbsp06",
         "imgSrc": "PersoonlijkR/workshop1js.PNG",
         "imgAlt": "",
-        "desc": "Voor de eerste smart connect workshop heb ik geleerd met javascript te gaan werken en hoe je daar gebruikt maakt van api naar ander site om gegevens in te laden.",
+        "desc": "Voor de eerste smart connect workshop heb ik geleerd met javascript te gaan werken en hoe je daar gebruikt maakt van api naar ander site om gegevens in te laden. Zelf heb ik nooit eerder met javascript gewerkt en vooral niet het inladen van elemten. Deze nieuwe kennis kon ik toepassen in het project en de portefolie. ",
         "path": "\\Smart Connect\\Precentaite..md"
     },
     {
@@ -126,7 +128,7 @@ var timelineCards2 = [
         "point": "week&nbsp07",
         "imgSrc": "PersoonlijkR/pointcloud.PNG",
         "imgAlt": "",
-        "desc": "Tijdens de presentatie van Bolai Xin heb ik geleerd hoe object detectie te werkt gaat achter de schermen, maar ook hoe je makkelijk in een python notebook te werk kan gaan. om zo je point cloud in te laden en te bewerken.",
+        "desc": "Tijdens de presentatie van Bolai Xin heb ik geleerd hoe object detectie te werkt gaat achter de schermen, maar ook hoe je makkelijk in een python notebook te werk kan gaan. om zo je point cloud in te laden en te bewerken. Dit is een van de lastigere onderwerpen waar ik nog steeds moeite mee heb om in een bruikbare casus te kunnen plaatsen.",
         "path": "\\Smart Connect\\Presetnetie opdracht plant connect,.md"
     },
     {
@@ -144,7 +146,7 @@ var timelineCards2 = [
         "point": "week&nbsp07",
         "imgSrc": "PersoonlijkR/backjev5.PNG",
         "imgAlt": "",
-        "desc": "Tijdens de workshoppen van 3d printen heb ik leren werken met een nieuwe 3d modeling programma, om zo mijn bakje voor de planten op dracht te ontwerken en te maken. Hiernaar heb ik ook nog een hoop nieuwe begrippen en technieken over 3d printen bij geleerd. Samen met Ashley,Stefano, en Bas​ hebben wij ook samen gewerkt aan een presentatie over het gebruik van 3d printen bij R&D",
+        "desc": "Tijdens de workshoppen van 3d printen heb ik leren werken met een nieuwe 3d modeling programma, om zo mijn bakje voor de planten opdracht te ontwerken en te maken. Hiernaar heb ik ook nog een hoop nieuwe begrippen en technieken over 3d printen bij geleerd. Samen met Ashley,Stefano, en Bas​ hebben wij ook samen gewerkt aan een presentatie over het gebruik van 3d printen bij R&D",
         "path": "\\Smart industry\\prestinetin opdracht\\presenatie 3d printen..md"
     },
     {
@@ -165,22 +167,31 @@ var timelineCards2 = [
         "desc": "Tijdens de influx lessen heb ik geleerd hoe je met een ESP32 de wifi kan ververbindne om op die manier key value pare data op te sturen naar een influx data base. Dit is tech echt anders mannier van te werk gegaan dan als ik gewend was, want je onthaald live data van sensor. Dit is een stuk anders dan de gewende manier van database gebruik waar bij je een website host die in de backend verbonden is met een database en schema.",
         "path": "\\Smart Technology\\Influx.md"
     },
-    {
-        "date": "2023-04-20",
-        "title": "prestnatine matthijs machines",
-        "point": "week&nbsp16",
-        "imgSrc": "",
-        "imgAlt": "",
-        "desc": "bij het inrehcten van je productie lijn moet je regeking houden met de buffers die je neer zet, voor de tijd dat machienn onbeheerd hun gang kunnen laten gaan. \n\nje moet ook op letten dat hetvor kan komen dat personeel, werk vercihten dat je niet direct door hebt. zo als doppen opdraaien die net niet goed zitten, of fouten flessen er uit halen. als je deze extra handelingen niet voor ogen hebt kun probleem krijgke ndat je betaaplde process niet heb afgevangen. \n\nhet aanpassen van bestande fabriek lijnen op processen die mis kunnen lopen kunnen lasting worden opgelost.",
-        "path": "\\Smart Technology\\prestnatine matthijs machines.md"
-    },
+    // {
+    //     "date": "2023-04-20",
+    //     "title": "prestnatine matthijs machines",
+    //     "point": "week&nbsp16",
+    //     "imgSrc": "",
+    //     "imgAlt": "",
+    //     "desc": "bij het inrehcten van je productie lijn moet je regeking houden met de buffers die je neer zet, voor de tijd dat machienn onbeheerd hun gang kunnen laten gaan. \n\nje moet ook op letten dat hetvor kan komen dat personeel, werk vercihten dat je niet direct door hebt. zo als doppen opdraaien die net niet goed zitten, of fouten flessen er uit halen. als je deze extra handelingen niet voor ogen hebt kun probleem krijgke ndat je betaaplde process niet heb afgevangen. \n\nhet aanpassen van bestande fabriek lijnen op processen die mis kunnen lopen kunnen lasting worden opgelost.",
+    //     "path": "\\Smart Technology\\prestnatine matthijs machines.md"
+    // },
     {
         "date": "2023-02-16",
         "title": "workshop 1 technologies",
         "point": "week&nbsp07",
         "imgSrc": "PersoonlijkR/esp32.jpeg",
         "imgAlt": "",
-        "desc": "Tijdens de workshop van smart technologies hebben we geleerd te werken met het programmeren van een ESP. Hierbij heb ik PlatformIO geleerd om zo in C++ codeing op de ESP.",
+        "desc": "Tijdens de workshop van smart technologies hebben we geleerd te werken met het programmeren van een ESP. Hierbij heb ik PlatformIO geleerd om zo in C++ codeing op de ESP. Omdat ik al wat kennis van embed software had ben ik hierverder in gaan verdiepen in het gebruik van infra rood leds waar mee ik mijn eigen thuis lampen kan aansturen, door de sinaal van de afstanbeindng uit te lezen en die te reprodiseren. Ook ben ik RFID cards bezig geweest om die te kunnen uitlezen en schrijven, waarbij je moest uit zoeken hoe de spesifictie werkt van zoon chip",
+        "path": "\\Smart Technology\\workshop 1.md"
+    },
+    {
+        "date": "2023-06-07",
+        "title": "Project",
+        "point": "week&nbsp19",
+        // "imgSrc": "",
+        "imgAlt": "",
+        "desc": "Het project van de minor was en is nog steeds de grote uitdaging die ik heb ondernomen tijdens de minor. Mijn project team van ik Joris en Chantal zijn begonen met een project wat al snel uit loopte tot veel veder gaande doel dan de de MSI minor voor ogen had. Dit is namelijk het opstarten van een eigen bedrijf met een product op de markt brengen. Zelf vind ik dit zeer spanned, want voorgaande projecen hadden altijd een vaste eind datum waar het klaar is en dat je er van af bent. Maar deze opdracht juist niet. Waarbij je binnen een school project in een simulatie zit waar je fouten mag maken zonder risico, is het opstellen van een eigen bedrijf niet zonder gevolgen. Dit valt exstreem buiten mijn eigen comfort zone en is nog steeds ongemaklijke en zeuwen wachtig. Gelukkig heb ik 2 geweldige studenten die mijn helpen hiermee, en achtief met ze drieren werken om mij zelf en elkaar elke dag te verbeteren. <br> Een voorbeeld van wat ik al heb opgemekt: <i>Wanneer ik een presetatie geeft en ik heb het naar mijn zin, dan blijf ik door klentsen. Dankzij de eerdere presentaties en de feedback die ik van mijn team genoten heb ontvangen, wist ik dat ik aan het doorratelen was en een einde moest maken. Dit was goed want ik ging zo als gewonlijk veel te diep op een punt in wat totaal niet de bedoeling was.</i><br> Maar ook met comunicatie zie ik verbetering, waarbij ik in het verleden niet zal hebben gereageerd; of zal hebben uitgesteld. Beantwoord en stel ik nu juist vragen.",
         "path": "\\Smart Technology\\workshop 1.md"
     }
 ];
